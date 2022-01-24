ab378e01432d8d13d38e6ce37a9dfeed17cc14cd
fernfd <kendrawow123@gmail.com>
2022-01-24 11:10:18 +0700

    wida

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
- 
+  return (
+      <div className='navbar'>
+          <div className='container'>
+              <div className='w-3/12'></div>
+                  <div className='Logo text-center uppereces text-2xl'>
+                      wida
+              </div>
+              <div className='w-6/12'></div>
+                  <div className='nav'>
+              
+              </div>
+              <div className='w-3/12'></div>
+              <div className='button'>
+          
+              </div>
+      
+    </div>
+    </div>
+  );
 }
 
 export default App;
